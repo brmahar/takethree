56750247687243bac27e08472b53ce894ba4c69e
Austin Browne <dazztrazak@gmail.com>
2014-03-02 01:15:50 -0500

    gun be bad

--- Socialize/src/com/lovrhatr/socialize/EventActivity.java
+++ Socialize/src/com/lovrhatr/socialize/EventActivity.java
@@ -1,15 +1,33 @@
 package com.lovrhatr.socialize;
 
+import com.parse.Parse;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class EventActivity extends Activity {
+	
+	private TextView title;
+	private TextView creator;
+	private TextView date;
+	private TextView time;
+	private TextView atendees;
+	private TextView description;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Parse.initialize(this, "ZNUGpdAW35nGYe5hvleBl3IndIphZPbZjVfn8Vcn", "JGTTBGebCrZXx2J2nc6TVh3is6bwUBq5hROkFCSI");
 		setContentView(R.layout.activity_event);
+		
+		title = (TextView)findViewById(R.id.editText1);
+		creator = (TextView)findViewById(R.id.editText2);
+		date = (TextView)findViewById(R.id.editText3);
+		time = (TextView)findViewById(R.id.editText4);
+		atendees = (TextView)findViewById(R.id.TextView01);
+		description = (TextView)findViewById(R.id.editText5);
 	}
 
 	@Override
